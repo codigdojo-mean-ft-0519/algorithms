f87de1fd4fa022c9d7fde12140cad73119a8abf5
Kent Hervey <kent.hervey@gmail.com>
2019-05-13 11:02:13 -0500

    complete

--- linkedLists/051319-nt-oo-kh.js
+++ linkedLists/051319-nt-oo-kh.js
@@ -1,6 +1,6 @@
 
 
-class SLNode {
+class Node {
     constructor(value) {
         this.val = value;
         this.next = null;
@@ -8,90 +8,120 @@ class SLNode {
 }
 
 
-class SList {
+class List {
     constructor() {
         this.head = null;
-        this.back = function () {
-            if (!this.head) {
-                return null;
-            }
-            var runner = this.head;
-            while (runner.next) {
-                runner = runner.next;
-            }
-            return runner.val;
-        };
-        this.pushBack = function (value) {
-            var newNode = new SLNode(value);
-            if (!this.head) {
-                this.head = newNode;
-            }
-            else {
-                var runner = this.head;
-                while (runner.next) {
-                    runner = runner.next;
-                }
-                runner.next = newNode;
-            }
-        };
-        this.popBack = function () {
-            if (!this.head) {
-                return null;
-            }
-            var returnVal;
-            if (!this.head.next) {
-                returnVal = this.head.val;
-                this.head = null;
-                return returnVal;
-            }
-            var runner = this.head;
-            while (runner.next.next) {
-                runner = runner.next;
-            }
-            returnVal = runner.next.val;
-            runner.next = null;
+    }
+
+    popBack () {
+        if (!this.head) {
+            return null;
+        }
+        var returnVal;
+        if (!this.head.next) {
+            returnVal = this.head.val;
+            this.head = null;
             return returnVal;
-        };
-        this.pushFront = function (value) {
-            var oldHead = this.head;
-            this.head = new SLNode(value);
-            this.head.next = oldHead;
-        };
-        this.popFront = function () {
-            var returnVal = null;
-            if (this.head) {
-                returnVal = this.head.val;
-                this.head = this.head.next;
-            }
-            return returnVal;
-        };
-        this.contains = function (value) {
-            var runner = this.head;
-            while (runner) {
-                if (runner.val === value) {
-                    return true;
-                }
-                runner = runner.next;
-            }
-            return false;
-        };
-        this.removeVal = function (value) {
-            if (!this.head) {
-                return false;
-            }
-            if (this.head.val === value) {
-                this.head = this.head.next;
+        }
+        var runner = this.head;
+        while (runner.next.next) {
+            runner = runner.next;
+        }
+        returnVal = runner.next.val;
+        runner.next = null;
+        return returnVal;
+    }
+    pushFront(value) {
+        var oldHead = this.head;
+        this.head = new Node(value);
+        this.head.next = oldHead;
+    }
+    popFront() {
+        var returnVal = null;
+        if (this.head) {
+            returnVal = this.head.val;
+            this.head = this.head.next;
+        }
+        return returnVal;
+    }
+    contains(value) {
+        var runner = this.head;
+        while (runner) {
+            if (runner.val === value) {
                 return true;
             }
+            runner = runner.next;
+        }
+        return false;
+    }
+    removeVal(value) {
+        if (!this.head) {
+            return false;
+        }
+        if (this.head.val === value) {
+            this.head = this.head.next;
+            return true;
+        }
+        var runner = this.head;
+        while (runner.next) {
+            if (runner.next.val === value) {
+                runner.next = runner.next.next;
+                return true;
+            }
+            runner = runner.next;
+        }
+        return false;
+    }
+
+    back () {
+        if (!this.head) {
+            return null;
+        }
+        var runner = this.head;
+        while (runner.next) {
+            runner = runner.next;
+        }
+        return runner.val;
+    }
+    pushBack (value) {
+        var newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+        }
+        else {
             var runner = this.head;
             while (runner.next) {
-                if (runner.next.val === value) {
-                    runner.next = runner.next.next;
-                    return true;
-                }
                 runner = runner.next;
             }
-            return false;
-        };
+            runner.next = newNode;
+        }
     }
+
+
+
+
 }
+
+const list = new List();
+console.log(list);
+console.log(`this is objname.instance variable or field:  ${list.head}`);
+list.pushFront("Kent");
+list.pushFront("Nina");
+
+console.log(`this is list.head:  `,list.head);
+
+console.log(`this is list.head.next:  `,list.head.next);
+
+//we have six functions we need to turn into methods:
+/*
+List {
+  head: null,
+  pushBack: [Function],
+  popBack: [Function],
+  pushFront: [Function],
+  popFront: [Function],
+  contains: [Function],
+  removeVal: [Function] }
+
+*/
+
